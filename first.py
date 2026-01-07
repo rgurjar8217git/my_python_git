2926ab8e2d34600b3a4549f81015786050f4c308
Rahul Gurjar <rgurjar8217-gif>
2026-01-07 10:07:37 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,18 @@
+s1 = input("Enter Your First Name : ")
+s2 = input("Enter Your Last Name : ")
+
+print("Hello,",s1 + s2 ,"! Welcome to Python Programming")
+
+
+num1 = int(float(input("Enter First Number :")))
+num2 = int(float(input("Enter Second Number :")))
+addition = num1 + num2
+print("The Addition is :",addition)
+subtraction = (num1-num2) 
+print("The Subtraction is :",subtraction)
+multiplication = num1 * num2
+print("The Multiplication is :",multiplication)
+division = num1 / num2
+print("The Division is :",division)
+modulus = num1 % num2
+print("The Modulus is :",modulus)
