df987f4d64f8d2b326cf67401f49b7712856c4a2
rgurjar8217git <rgurjar8217@gmail.com>
2026-01-07 16:09:55 +0530

    added new assignment file

---
+++ assignment/second assignment .py
@@ -0,0 +1,13 @@
+num = int(input("Enter an number : "))
+if num % 2==0:
+    print(num," is an even")
+else:
+    print(num," is an odd")
+
+
+#for an loop 
+total = 0
+for i in range(1,51):
+    total +=i
+
+    print("The sum of 1 to 50 is: ",total) 
